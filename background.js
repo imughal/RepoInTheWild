4cc772d8636259b5c355e8120bac558683439670
Matthias Gliwka <mg@cerebuild.com>
2017-09-09 16:49:53 +0200

    Extend caching to websites without public repos

--- background.js
+++ background.js
@@ -6,6 +6,8 @@ var detectionPaths = {
   hg: ['.hg/requires']
 }
 
+var repoTypeCount = Object.keys(detectionPaths).length
+
 var allowedProtocols = ["https:", "http:"]
 
 function checkURLReturns200(url, callback) {
@@ -32,10 +34,13 @@ function checkForRepository(url, tabId) {
 
   var hostWithProtocol = parsedUrl.protocol + '//' + parsedUrl.host
 
-  if(!sessionCache[hostWithProtocol]) {
+  if(sessionCache[hostWithProtocol] === undefined) {
+    var alreadyChecked = []
     Object.keys(detectionPaths).forEach(function(repoType) {
       detectionPaths[repoType].forEach(function(path) {
         checkURLReturns200(hostWithProtocol + '/' + path, function(success) {
+          alreadyChecked.push(repoType)
+
           if(success) {
             sessionCache[hostWithProtocol] = repoType
             //make sure it's still the same page in the tab after the request completed
@@ -45,11 +50,16 @@ function checkForRepository(url, tabId) {
               }
             })
           }
+          else if(alreadyChecked.length === repoTypeCount && sessionCache[hostWithProtocol] === undefined)
+          {
+            //set it to false to make sure it doesn't get queried again during this session
+            sessionCache[hostWithProtocol] = false
+          }
         })
       })
     })
   }
-  else {
+  else if(sessionCache[hostWithProtocol] !== false){
     setIcon(sessionCache[hostWithProtocol], tabId)    
   }
 }
